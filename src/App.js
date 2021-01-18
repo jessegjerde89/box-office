d54a94e7f002f107cde4f5964657f1e6c5d8a1ab
Jesse Gjerde <jesseg@MacBook-Pro.local>
2021-01-18 11:33:01 -0700

    routes started

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
 function App() {
-  return <div>Hi</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        This is the home page
+      </Route>
+
+      <Route exact path="/start"> This is starred
+      </Route>
+
+    <Route>
+      This is a 404 page 
+    </Route>
+
+    </Switch>
+  );
 }
 
 export default App;
