43f55d1e1b5b1f4b62e1b69e2c9c0bd3578c0bad
Jesse Gjerde <jesseg@MacBook-Pro.local>
2021-01-18 20:26:45 -0700

    more layout work done

--- src/pages/Starred.js
+++ src/pages/Starred.js
@@ -1,7 +1,8 @@
 import React from "react";
+import MainPageLayout from "../components/MainPageLayout";
 
 export const Starred = () => {
-  return <div>this is Starred</div>;
+  return <MainPageLayout>this is Starred</MainPageLayout>;
 };
 
 export default Starred;
