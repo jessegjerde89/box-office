f8a931f1021f9ad4f39cc44d7d1b7ee3fb705379
Jesse Gjerde <jesseg@MacBook-Pro.local>
2021-01-19 20:14:05 -0700

    the show page being updated from an api

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Starred from "./pages/Starred";
+import Show from "./pages/Show";
 
 function App() {
   return (
@@ -18,6 +19,10 @@ function App() {
 
         <Route>This is a 404 page</Route>
       </Switch>
+
+      <Route exact path="/show/id">
+        <Show />
+      </Route>
     </div>
   );
 }
